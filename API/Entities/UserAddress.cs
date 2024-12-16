2219d31f1367e0715166b9c55dc8d5157aa6e5ad
simon <simonguo.eng@yahoo.com>
2024-12-16 06:37:21 -0500

    add order controller

--- API/Entities/UserAddress.cs
+++ API/Entities/UserAddress.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +9,9 @@ namespace API.Entities
 {
     public class UserAddress : Address
     {
+        [Key]
         public int Id { get; set; }
+        [ForeignKey("UserId")]
         public int UserId { get; set; }
         public User User { get; set; }
         public bool IsDefault { get; set; }
